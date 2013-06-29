5aae73afd6c26e434ea7b931ee35f1d2be754955
jack <appdev00001@gmail.com>
2013-06-29 20:43:43 +0800

    modify post view

--- sgdiscover/protected/views/post/_view.php
+++ sgdiscover/protected/views/post/_view.php
@@ -4,8 +4,8 @@
 ?>
 
 <div class="view">
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+        
+	<!--<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
 	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
 	<br />
 
@@ -31,7 +31,65 @@
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('content')); ?>:</b>
 	<?php echo CHtml::encode($data->content); ?>
-	<br />
+	<br />-->
+        
+   
+<?php /*$box = $this->beginWidget('bootstrap.widgets.TbBox', array(
+    'title' => CHtml::encode($data->title),
+    'headerIcon' => 'icon-th-list',
+    // when displaying a table, if we include bootstra-widget-table class
+    // the table will be 0-padding to the box
+    'htmlOptions' => array('class'=>'bootstrap-widget-table')
+));*/ ?>
+<!--
+<table class="table">
+    <thead>
+    <tr>
+        <th>#</th>
+    </tr>
+    </thead>--
+    <tbody>
+    <tr>
+        <td><i class='icon-th-list'></i> 20</td>
+    </tr>
+        
+    </tbody>
+</table>
+--> 
+<?php /*$this->endWidget();*/?>
+  
+ 
+<table  class='table'>
+	<tr>
+		<td colspan=2>
+			<h3>
+				<?php echo CHtml::link(CHtml::encode($data->title), array('view', 'id'=>$data->id)); ?>							
+			</h3>
+		</td>
+	</tr>
+	<tr>
+		<td><table class='table table-bordered table-striped list'>
+                        <tr>
+                                <td><i class="icon-calendar icon-small"></i> <?php echo CHtml::encode($data->begin_time); ?> to <?php echo CHtml::encode($data->end_time); ?></td>
+                                <!--<td rowspan='5'
+                                        style='width: 170px; padding-right: 20px; vertical-align: top;'
+                                        class='hideMobile'><a href="/event/view/id/165"><img class="thumbnail" src="/images/cover/thumbnail165.png" alt="新加坡英语口语聚会" /></a></td>-->
+                        </tr>
+                        <tr>
+                                <td><i class="icon-map-marker icon-large"></i> <a target='_blank'
+                                        href='https://maps.google.com/maps?output=embed&q=Singapore+203+Hougang+Street+21+%2304-75'>203 Hougang Street 21 #04-75					</a></td>
+                        </tr>                     
+                        <!--<tr>
+                                <td><i class="icon-phone-sign icon-small"></i> 新知家 / <a href='tel:92345238'>92345238</a>					</td>
+                        </tr>-->
+		</table></td>
+  
+	</tr>
+	</table> 
+
+
+
+ 
 
 	<?php /*
 	<b><?php echo CHtml::encode($data->getAttributeLabel('tags')); ?>:</b>
@@ -62,6 +120,7 @@
 	<?php echo CHtml::encode($data->author_id); ?>
 	<br />
 
-	*/ ?>
+	 */?>
+
 
 </div>
--- sgdiscover/protected/views/post/index.php
+++ sgdiscover/protected/views/post/index.php
@@ -11,44 +11,13 @@ $this->menu=array(
 	array('label'=>'Manage Post', 'url'=>array('admin')),
 );
 ?>
+
+
 <?php if(!empty($_GET['tag'])):?>
 <h1>Posts tagged with <i><?php echo CHtml::encode($_GET['tag']);?></i></h1>
 <?php endif; ?>
 
-
-<?php $box = $this->beginWidget('bootstrap.widgets.TbBox', array(
-    'title' => 'NB盒子',
-    'headerIcon' => 'icon-th-list',
-    // when displaying a table, if we include bootstra-widget-table class
-    // the table will be 0-padding to the box
-    'htmlOptions' => array('class'=>'bootstrap-widget-table')
-));?>
-<table class="table">
-    <thead>
-    <tr>
-        <th>#</th>
-        <th>First name</th>
-        <th>Last name</th>
-        <th>Language</th>
-        <th>Hours worked</th>
-    </tr>
-    </thead>
-    <tbody>
-        <tr class="odd">
-        <td>1</td><td>Mark</td><td>Otto</td><td>css</td><td>10</td>
-        </tr>
-        <tr class="even">
-        <td>2</td><td>Jacob</td><td>Thornton</td><td>JavaScript</td><td>20</td>
-        </tr>
-        <tr class="odd">
-        <td>3</td><td>Stu</td><td>Dent</td><td>HTML</td><td>15</td>
-        </tr>
-    </tbody>
-</table>
-<?php $this->endWidget();?>
-
-
-
+<!--  
 <?php $this->widget('bootstrap.widgets.TbGridView', array(
     'type'=>'striped bordered condensed',
     'dataProvider'=>$dataProvider,
@@ -64,9 +33,9 @@ $this->menu=array(
         ),
     ),
 )); ?>
+-->
 
-
-
+<!--
 <?php $this->widget('bootstrap.widgets.TbDetailView', array(
     'data'=>array('id'=>1, 'firstName'=>'Mark', 'lastName'=>'Otto', 'language'=>'CSS'),
     'attributes'=>array(
@@ -74,7 +43,7 @@ $this->menu=array(
         array('name'=>'lastName', 'label'=>'Last name'),
         array('name'=>'language', 'label'=>'Language'),
     ),
-)); ?>
+)); ?>-->
 
 
 <?php $this->widget('zii.widgets.CListView', array(
@@ -82,8 +51,3 @@ $this->menu=array(
 	'itemView'=>'_view',
         'template' => "{items}\n{pager}",
 )); ?>
-
-
-
-
-<?php 
