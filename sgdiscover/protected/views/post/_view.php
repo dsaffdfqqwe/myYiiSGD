c8d7a2e18415dbb3f99e06b2ae73e1aab4062584
jack <appdev00001@gmail.com>
2013-07-13 21:33:29 +0800

    hoho

--- sgdiscover/protected/views/post/_view.php
+++ sgdiscover/protected/views/post/_view.php
@@ -3,46 +3,39 @@
 /* @var $data Post */
 ?>
 
-<div class="span12">
-              
-	
-	
-		
-                    <table class='table table-bordered table-striped list'>
-                        <tbody>
-                         <tr>
-		<td colspan="2">
-				
-	        <?php echo CHtml::link(CHtml::encode($data->title), array('view', 'id'=>$data->id)); ?>	
-                    	
-			
+<!--<div class="span12">-->
+<table class='table'><tr>    
+    <table class='table table-bordered table-striped list'>
+        <tbody>
+             <tr>
+                <td  colspan="2">
+                <?php echo CHtml::link(CHtml::encode($data->title), array('view', 'id'=>$data->id)); ?>	
                 </td>		
-	</tr>   
-                        <tr>
-                            <td style="background: white"><i class="icon-calendar"></i> <?php echo CHtml::encode(date('m-d',$data->day)); ?> <?php echo CHtml::encode(date('gA',$data->time)); ?>
-                                </td>
-                                <td rowspan='5'
-                                        style='width: 300px; padding-right: 20px; vertical-align: top;'
-                                        class='hideMobile'><a href=<?php echo CHtml::link("", array('view', 'id'=>$data->id)); ?><img class="thumbnail" src="http://127.0.0.1/yii/sgdiscover/images/pic/<?php echo mt_rand(1,65)?>.jpg" alt="" /></a></td>
-                        </tr>
-                        <tr>
-                                <td style="background: white"><i class="icon-map-marker"></i> <a target='_blank'
-                                        href='https://maps.google.com/maps?output=embed&q=singapore <?php echo CHtml::encode($data->location); ?>'><?php echo CHtml::encode($data->location); ?></a>
-                                </td>
-                        </tr>    
-                        <tr>
-                                <td  style="background: white"><i class="icon-edit"></i><?php echo CHtml::encode($data->content); ?>
-                                </td>
-                        </tr>  
-                        
-                        <!--<tr>
-                                <td><i class="icon-phone-sign icon-small"></i> 新知家 / <a href='tel:92345238'>92345238</a>					</td>
-                        </tr>-->
-                        </tbody>
-                    </table>
-                
-  
-	
+            </tr>   
+            
+            <tr>
+                <td ><i class="icon-calendar"></i> <?php echo CHtml::encode(date('m-d',$data->day)); ?> <?php echo CHtml::encode(date('gA',$data->time)); ?>
+                </td>
+                <td rowspan='5'
+                            style='width: 300px; padding-right: 20px; vertical-align: top;'
+                            class='hideMobile'><a href=<?php echo CHtml::link("", array('view', 'id'=>$data->id)); ?><img class="thumbnail" src="http://127.0.0.1/yii/sgdiscover/images/pic/<?php echo mt_rand(1,65)?>.jpg" alt="" /></a>
+                </td>
+            </tr>
+            
+            <tr>
+                    <td><i class="icon-map-marker"></i> <a target='_blank'
+                            href='https://maps.google.com/maps?output=embed&q=singapore <?php echo CHtml::encode($data->location); ?>'><?php echo CHtml::encode($data->location); ?></a>
+                    </td>
+            </tr>    
+            <tr>
+                    <td><i class="icon-edit"></i><?php echo CHtml::encode($data->content); ?>
+                    </td>
+            </tr>  
 
-
-</div>
+            <!--<tr>
+                    <td><i class="icon-phone-sign icon-small"></i> 新知家 / <a href='tel:92345238'>92345238</a>					</td>
+            </tr>-->
+         </tbody>
+    </table></tr>
+</table>    
+<!--</div>-->
